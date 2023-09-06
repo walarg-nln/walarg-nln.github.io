71577eb1df55400d7ca58dc6d603e134a74d096d
walker argao <walker.argao@nextlevelnola.org>
2023-09-06 18:44:36 +0000

    refactor to create 100 circles using loops

--- projects/platformer/setup.js
+++ projects/platformer/setup.js
@@ -1,7 +1,7 @@
 // setup variables
 const walkAcceleration = 7; // how much is added to the speed each frame
 const gravity = 1; // how much is subtracted from speedY each frame
-const friction = 0.7; // how much the player is slowed each frame
+const friction = 0.2; // how much the player is slowed each frame
 const maxSpeed = 16; // maximum horizontal speed, not vertical
 const playerJumpStrength = 25; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
