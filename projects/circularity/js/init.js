a190c0e1b45ee04e162c2011bbd425850a44e9c0
walker argao <walker.argao@nextlevelnola.org>
2023-09-06 18:41:51 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,45 +1,45 @@
 var init = function (window) {
     'use strict';
-    var 
+    var
         draw = window.opspark.draw,
         physikz = window.opspark.racket.physikz,
-        
+
         app = window.opspark.makeApp(),
-        canvas = app.canvas, 
+        canvas = app.canvas,
         view = app.view,
         fps = draw.fps('#000');
-        
-    
-    window.opspark.makeGame = function() {
-        
+
+
+    window.opspark.makeGame = function () {
+
         window.opspark.game = {};
         var game = window.opspark.game;
-        
+
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM SETUP ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+
         // TODO 1 : Declare and initialize our variables
         var circle;
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle () {
-            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, false, "#999", 2);
             physikz.addRandomVelocity(circle, canvas, 3, 3);
             view.addChild(circle);
             circles.push(circle);
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for ( var i = 1; i <= 100; i++) {
+        for (var i = 1; i <= 100; i++) {
             drawCircle();
         }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+
         /* 
         This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
@@ -47,31 +47,31 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-           // the functions here did not follow the dry method.
-           // I totally wrote them and then just deleted them.
-            
+            // the functions here did not follow the dry method.
+            // I totally wrote them and then just deleted them.
+
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            
-           
+
+
 
             // TODO 9 : Iterate over the array
-            for ( var i = 0; i < circles.length; i++) {
+            for (var i = 0; i < circles.length; i++) {
                 physikz.updatePosition(circles[i]);
             }
-            for ( var i = 0; i < circles.length; i++) {
+            for (var i = 0; i < circles.length; i++) {
                 game.checkCirclePosition(circles[i]);
             }
         }
-    
+
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-        game.checkCirclePosition = function(circle) {
+        game.checkCirclePosition = function (circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x + circle.radius > canvas.width ) {
+            if (circle.x + circle.radius > canvas.width) {
                 circle.x = circle.radius;
             }
             // if the circle hits the left side of screen send it to right side
@@ -82,35 +82,35 @@ var init = function (window) {
             if (circle.y - circle.radius < 0) {
                 circle.y = canvas.height - circle.radius;
             }
-             // if circle hits the bottom of the screen send it to the top
+            // if circle hits the bottom of the screen send it to the top
             if (circle.y + circle.radius > canvas.height) {
                 circle.y = circle.radius;
             }
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
-        
+
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
         /////////////////////////////////////////////////////////////
-        
+
         view.addChild(fps);
         app.addUpdateable(fps);
-        
+
         game.circle = circle;
         game.circles = circles;
         game.drawCircle = drawCircle;
         game.update = update;
-        
+
         app.addUpdateable(window.opspark.game);
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
--- projects/platformer/setup.js
+++ projects/platformer/setup.js
@@ -1,9 +1,9 @@
 // setup variables
-const walkAcceleration = 2.5; // how much is added to the speed each frame
-const gravity = 0.5; // how much is subtracted from speedY each frame
-const friction = 1.5; // how much the player is slowed each frame
-const maxSpeed = 8; // maximum horizontal speed, not vertical
-const playerJumpStrength = 12; // this is subtracted from the speedY each jump
+const walkAcceleration = 7; // how much is added to the speed each frame
+const gravity = 1; // how much is subtracted from speedY each frame
+const friction = 0.7; // how much the player is slowed each frame
+const maxSpeed = 16; // maximum horizontal speed, not vertical
+const playerJumpStrength = 25; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 
 /////////////////////////////////////////////////
